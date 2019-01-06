4049a74aaf92287b2561d17770039544697ba8c1
luckytyphlosion <10688458+luckytyphlosion@users.noreply.github.com>
2019-01-06 12:43:12 -0500

    Give Blastoise Thunderbolt.

--- data/baseStats/blastoise.asm
+++ data/baseStats/blastoise.asm
@@ -20,7 +20,7 @@ db 3 ; growth rate
 ; learnset
 	tmlearn 1,5,6,8
 	tmlearn 9,10,11,12,13,14,15
-	tmlearn 17,18,19,20
+	tmlearn 17,18,19,20,24
 	tmlearn 26,27,28,31,32
 	tmlearn 33,34,40
 	tmlearn 44
